51683476fdf8b3a19c8d8a893e83d587edd4a759
Melvin Lindholm <116351074+MelvinLTe21C@users.noreply.github.com>
2023-01-20 01:28:27 +0100

    finalized file

--- utils/item.py
+++ utils/item.py
@@ -2,13 +2,13 @@ class Item:
     def __init__(self, item_num):
         """Creates an item object. input the number the item is on in items.txt.
         Items can be weapons, potions or relics, and have different attributes based on what they are."""
-        item_file = open("items.txt")
+        item_file = open("items.txt", encoding="utf-8")
         items = item_file.readlines()
-        item_info = items[item_num].split(",")
+        item_info = items[item_num - 1].split(",")
         self.name = item_info[0]
         if item_info[1].endswith("hp"):
-            self.type = "Potion"
-            self.healthGain = item_info[1][0:2]
+            self.type = "potion"
+            self.healthGain = item_info[1].replace("hp", "")
         else:
-            self.type = "Weapon"
+            self.type = "weapon"
             self.strengthBonus = int(item_info[1])
